aae7f5218065030a0c52150ada97ddd4e5d0bd1f
Pranav Mahajan <pranavma@in.ibm.com>
2020-01-30 15:12:54 +0530

    Changes

--- src/com/Pranav/Miscellaneous/VarArgsDemo.java
+++ src/com/Pranav/Miscellaneous/VarArgsDemo.java
@@ -4,11 +4,20 @@ public class VarArgsDemo {
 
 	public static void main(String[] args) {
 		Calc c = new Calc();
-		System.out.println(c.sum(20, 30));
+		System.out.println(c.sum(10,20,30,40,50));
 	}
 
 }
 class Calc{
+	int sum(int... args)
+	{
+		int summed=0;
+		for(Integer i: args)
+		{
+			summed=summed+i;
+		}
+		return summed;
+	}
 	int sum(int first, int second)
 	{
 		return first+second;
